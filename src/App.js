18365477ebc369e62db147dca234df66b0c726af
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2020-06-16 12:28:19 -0400

    Added images to the screen.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-import banner from './banner.png'
-
 function App() {
   return (
     <div className="App">
-    <img src={banner} alt="Banner" />
       <div className="topbar">
         <ul>
           <li>SPEAKERS</li>
@@ -13,9 +10,6 @@ function App() {
           <li>WORKSHOPS</li>
           <li>CONTACT</li>
         </ul>
-      </div>
-      <div className="upperhalf">
-
       </div>
       <div className="bottomleft">
       </div>
