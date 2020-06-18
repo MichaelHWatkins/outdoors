956df0dbc5403de29b88348883bb8acb6cf540e7
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2020-06-18 13:53:18 -0400

    Added some css to make the font white and centered. Added two other buttons to finish everything but the final requirement.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 function App() {
+  const textArray = ['Huge and powerful statement',
+	'Still a powerful statement',
+	'Very interesting and profound headline']
+
+  const [seconds, setSeconds] = useState(0);
+  const [phrase, setPhrase] = useState("Test")
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+      setPhrase(textArray[seconds % textArray.length]);
+    }, 3000);
+  }, []);
+
+  const backpackingPhrase = event => {
+    event.preventDefault();
+    setPhrase("The right place for Backpacking lovers")
+  }
+
+  const campingPhrase = event => {
+    event.preventDefault();
+    setPhrase("The right place for Camping lovers")
+  }
+
+  const hikingPhrase = event => {
+    event.preventDefault();
+    setPhrase("The right place for Camping lovers")
+  }
+
   return (
     <div className="App">
       <div className="topbar">
@@ -10,12 +41,16 @@ function App() {
           <li>WORKSHOPS</li>
           <li>CONTACT</li>
         </ul>
+        <h1>{phrase}</h1>
       </div>
       <div className="bottomleft">
+        <h2><button onClick={backpackingPhrase}>Backpacking</button></h2>
       </div>
       <div className="bottomcenter">
+        <h2><button onClick={campingPhrase}>Camping</button></h2>
       </div>
       <div className="bottomright">
+        <h2><button onClick={hikingPhrase}>Hiking</button></h2>
       </div>
     </div>
   );
